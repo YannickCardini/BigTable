de841716603f1ca9953032b9060e340a45f03630
BenoitMtr <benoitm325@gmail.com>
2020-06-01 17:59:09 +0200

    ajout de multiples objets à la fois

--- src/main/java/setup/TasteOfRiak.java
+++ src/main/java/setup/TasteOfRiak.java
@@ -24,6 +24,7 @@ import main.java.util.ToolBox;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.text.ParseException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class TasteOfRiak {
@@ -31,7 +32,7 @@ public class TasteOfRiak {
 	// public static final String PATH = "/home/benoit/Documents/ressources/";
 	public static final String PATH = "/home/loubard/Documents/BD_BigTable/DATA/";
 
-	// TODO : modifier le nom du dossier #YannickMiage
+	// TODO : modifier le nom du dossier
 	public static final String PROJECT_PATH = ToolBox.getProjectDirectoryPath();
 	public static String dataPath;
 
@@ -43,8 +44,12 @@ public class TasteOfRiak {
 	// This will create a client object that we can use to interact with Riak
 	private static RiakCluster setUpCluster() throws UnknownHostException {
 
-		// This example will use only one node listening on localhost:10017
+		//si on veut se connecter à distance
 		RiakNode node = new RiakNode.Builder().withRemoteAddress("18.220.122.237").build();
+
+		//si on a une instance en local
+		//RiakNode node = new RiakNode.Builder().withRemoteAddress("127.0.0.1").withRemotePort(8087).build();
+
 
 		// This cluster object takes our one node as an argument
 	    RiakCluster cluster = new RiakCluster.Builder(node).build();
@@ -178,6 +183,119 @@ public class TasteOfRiak {
 
 	}
 
+	private static void addMultiplePerson(List<Person> listPers, RiakClient client)
+			throws InterruptedException {
+
+		Location loc;
+		RiakObject ro;
+		StoreValue storeOp;
+		Namespace bucket;
+
+		for(Person pers:listPers)
+		{
+			bucket = new Namespace("person", pers.getId());
+			if (pers.getBirthday() != null && pers.getBirthday().length() > 0) {
+				loc = new Location(bucket, "birthday");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(pers.getBirthday()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (pers.getFirstName() != null && pers.getFirstName().length() > 0) {
+				loc = new Location(bucket, "firstName");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(pers.getFirstName()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (pers.getLastName() != null && pers.getLastName().length() > 0) {
+				loc = new Location(bucket, "lastName");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(pers.getLastName()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (pers.getGender() != null && pers.getGender().length() > 0) {
+				loc = new Location(bucket, "gender");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(pers.getGender()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (pers.getCreateDate() != null && pers.getCreateDate().length() > 0) {
+				loc = new Location(bucket, "createDate");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(pers.getCreateDate()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (pers.getLocation() != null && pers.getLocation().length() > 0) {
+				loc = new Location(bucket, "location");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(pers.getLocation()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (pers.getBrowserUsed() != null && pers.getBrowserUsed().length() > 0) {
+				loc = new Location(bucket, "browserUsed");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(pers.getBrowserUsed()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (pers.getPlace() != null && pers.getPlace().length() > 0) {
+				loc = new Location(bucket, "place");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(pers.getPlace()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Insertion de "+pers.getId()+" réussie");
+		}
+
+
+
+	}
+
 	private static void updatePerson(String bucketName, String personKey, String newValue, RiakClient client)
 			throws ExecutionException, InterruptedException {
 
@@ -186,7 +304,6 @@ public class TasteOfRiak {
 				.build();
 		FetchValue.Response response = client.execute(fetch);
 		RiakObject obj = response.getValue(RiakObject.class);
-		System.out.println(obj.getValue());
 
 		System.out.println("Ancienne valeur: "+obj.getValue().toString());
 		obj.setValue(BinaryValue.create(newValue));
@@ -262,6 +379,36 @@ public class TasteOfRiak {
 
 	}
 
+	private static void addMultipleFeedback(List<Feedback> listFeed, RiakClient client)
+			throws InterruptedException {
+
+		Location loc;
+		RiakObject ro;
+		StoreValue storeOp;
+		Namespace bucket;
+		// car aucun des 2
+		// uniques
+		for(Feedback feed: listFeed)
+		{	bucket = new Namespace("feedback", feed.getAsin() + feed.getPersonId());// Combo asin +personId
+			if (feed.getFeedback() != null && feed.getFeedback().length() > 0) {
+				loc = new Location(bucket, "feed");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(feed.getFeedback()));
+				storeOp = new StoreValue.Builder(ro).withLocation(loc).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Insertion de "+feed.getAsin() + feed.getPersonId()+" réussie");
+		}
+
+
+
+
+	}
+
 	private static void updateFeedback(String bucketName, String feedBackKey, String newValue, RiakClient client)
 			throws ExecutionException, InterruptedException {
 
@@ -270,7 +417,6 @@ public class TasteOfRiak {
 				.build();
 		FetchValue.Response response = client.execute(fetch);
 		RiakObject obj = response.getValue(RiakObject.class);
-		System.out.println(obj.getValue());
 
 		System.out.println("Ancienne valeur: "+obj.getValue().toString());
 		obj.setValue(BinaryValue.create(newValue));
@@ -377,6 +523,71 @@ public class TasteOfRiak {
 
     }
 
+	private static void addMultipleProduct(List<Product> listProd, RiakClient client)
+			throws InterruptedException {
+
+		Location productLocation;
+		RiakObject ro;
+		StoreValue storeProductOp;
+		Namespace productBucket;
+
+		for(Product prod:listProd)
+		{
+			productBucket = new Namespace("product", prod.getAsin());
+			if (prod.getPrice() != null && prod.getPrice().length() > 0) {
+				productLocation = new Location(productBucket, "price");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(prod.getPrice()));
+				storeProductOp = new StoreValue.Builder(ro).withLocation(productLocation).build();
+				try {
+					client.execute(storeProductOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (prod.getTitle() != null && prod.getTitle().length() > 0) {
+				productLocation = new Location(productBucket, "title");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(prod.getTitle()));
+				storeProductOp = new StoreValue.Builder(ro).withLocation(productLocation).build();
+				try {
+					client.execute(storeProductOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (prod.getImgUrl() != null && prod.getImgUrl().length() > 0) {
+				productLocation = new Location(productBucket, "imgUrl");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(prod.getImgUrl()));
+				storeProductOp = new StoreValue.Builder(ro).withLocation(productLocation).build();
+				try {
+					client.execute(storeProductOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (prod.getBrand() != null && prod.getBrand().length() > 0) {
+				productLocation = new Location(productBucket, "brand");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(prod.getBrand()));
+				storeProductOp = new StoreValue.Builder(ro).withLocation(productLocation).build();
+				try {
+					client.execute(storeProductOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println("Insertion de "+prod.getAsin()+" réussie");
+		}
+
+
+	}
+
 	private static void updateProduct(String bucketName, String productKey, String newValue, RiakClient client)
 			throws ExecutionException, InterruptedException {
 
@@ -385,7 +596,6 @@ public class TasteOfRiak {
 				.build();
 		FetchValue.Response response = client.execute(fetch);
 		RiakObject obj = response.getValue(RiakObject.class);
-		System.out.println(obj.getValue());
 
 		System.out.println("Ancienne valeur: "+obj.getValue().toString());
 		obj.setValue(BinaryValue.create(newValue));
@@ -465,6 +675,44 @@ public class TasteOfRiak {
 
 	}
 
+	private static void addMultipleVendor(List<Vendor> listVend, RiakClient client)
+			throws InterruptedException
+	{
+		Location vendorLocation;
+		RiakObject ro;
+		StoreValue storeOp;
+		Namespace bucket;
+
+		for(Vendor vend:listVend)
+		{	bucket = new Namespace("vendor", vend.getVendor());
+			if (vend.getCountry() != null && vend.getCountry().length() > 0) {
+				vendorLocation = new Location(bucket, "country");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(vend.getCountry()));
+				storeOp = new StoreValue.Builder(ro).withLocation(vendorLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (vend.getIndustry() != null && vend.getIndustry().length() > 0) {
+				vendorLocation = new Location(bucket, "industry");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(vend.getIndustry()));
+				storeOp = new StoreValue.Builder(ro).withLocation(vendorLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+
+	}
+
 	private static void updateVendor(String bucketName, String vendorKey, String newValue, RiakClient client)
 			throws ExecutionException, InterruptedException {
 
@@ -473,7 +721,6 @@ public class TasteOfRiak {
 				.build();
 		FetchValue.Response response = client.execute(fetch);
 		RiakObject obj = response.getValue(RiakObject.class);
-		System.out.println(obj.getValue());
 
 		System.out.println("Ancienne valeur: "+obj.getValue().toString());
 		obj.setValue(BinaryValue.create(newValue));
@@ -577,6 +824,67 @@ public class TasteOfRiak {
 		}
 	}
 
+	private static void addMultipleInvoice(List<Order> listInv, RiakClient client)
+			throws InterruptedException
+	{
+		Location invoiceLocation;
+		RiakObject ro;
+		StoreValue storeOp;
+		Namespace bucket;
+
+		for(Order inv: listInv)
+		{	bucket = new Namespace("invoice", inv.getOrderId());
+			if (inv.getPersonId() != null && inv.getPersonId().length() > 0) {
+				invoiceLocation = new Location(bucket, "personId");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(inv.getPersonId()));
+				storeOp = new StoreValue.Builder(ro).withLocation(invoiceLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (inv.getOrderDate() != null && inv.getOrderDate().length() > 0) {
+				invoiceLocation = new Location(bucket, "orderDate");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(inv.getOrderDate()));
+				storeOp = new StoreValue.Builder(ro).withLocation(invoiceLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (inv.getTotalPrice() != null && inv.getTotalPrice().length() > 0) {
+				invoiceLocation = new Location(bucket, "totalPrice");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(inv.getTotalPrice()));
+				storeOp = new StoreValue.Builder(ro).withLocation(invoiceLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (inv.getOrderLine() != null && inv.getOrderLine().length() > 0) {
+				invoiceLocation = new Location(bucket, "totalPrice");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(inv.getOrderLine()));
+				storeOp = new StoreValue.Builder(ro).withLocation(invoiceLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
 	private static void updateInvoice(String bucketName, String invoiceKey, String newValue, RiakClient client)
 			throws ExecutionException, InterruptedException {
 
@@ -585,10 +893,8 @@ public class TasteOfRiak {
 				.build();
 		FetchValue.Response response = client.execute(fetch);
 		RiakObject obj = response.getValue(RiakObject.class);
-		System.out.println(obj.getValue());
 
 		System.out.println("Ancienne valeur: "+obj.getValue().toString());
-		obj.setValue(BinaryValue.create(newValue));
 
 		StoreValue updateOp = new StoreValue.Builder(obj)
 				.withLocation(invoiceLocation)
@@ -690,6 +996,67 @@ public class TasteOfRiak {
 		}
 	}
 
+	private static void addMultipleOrder(List<Order> listOrd, RiakClient client)
+			throws InterruptedException
+	{
+		Location vendorLocation;
+		RiakObject ro;
+		StoreValue storeOp;
+		Namespace bucket;
+
+		for(Order ord: listOrd)
+		{	bucket = new Namespace("order", ord.getOrderId());
+			if (ord.getPersonId() != null && ord.getPersonId().length() > 0) {
+				vendorLocation = new Location(bucket, "personId");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(ord.getPersonId()));
+				storeOp = new StoreValue.Builder(ro).withLocation(vendorLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (ord.getOrderDate() != null && ord.getOrderDate().length() > 0) {
+				vendorLocation = new Location(bucket, "orderDate");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(ord.getOrderDate()));
+				storeOp = new StoreValue.Builder(ro).withLocation(vendorLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (ord.getTotalPrice() != null && ord.getTotalPrice().length() > 0) {
+				vendorLocation = new Location(bucket, "totalPrice");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(ord.getTotalPrice()));
+				storeOp = new StoreValue.Builder(ro).withLocation(vendorLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (ord.getOrderLine() != null && ord.getOrderLine().length() > 0) {
+				vendorLocation = new Location(bucket, "totalPrice");
+				ro = new RiakObject();
+				ro.setValue(BinaryValue.create(ord.getOrderLine()));
+				storeOp = new StoreValue.Builder(ro).withLocation(vendorLocation).build();
+				try {
+					client.execute(storeOp);
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
 	private static void updateOrder(String bucketName, String orderKey, String newValue, RiakClient client)
 			throws ExecutionException, InterruptedException {
 
@@ -698,7 +1065,6 @@ public class TasteOfRiak {
 				.build();
 		FetchValue.Response response = client.execute(fetch);
 		RiakObject obj = response.getValue(RiakObject.class);
-		System.out.println(obj.getValue());
 
 		System.out.println("Ancienne valeur: "+obj.getValue().toString());
 		obj.setValue(BinaryValue.create(newValue));
@@ -756,28 +1122,28 @@ public class TasteOfRiak {
 
 			//Création d'un Product de test, pour l'insérer 
 			// test d'une query basique
-            /*Product testProd=new Product();
-            testProd.setAsin("testInsertion5");
-            testProd.setBrand("brandTest5");
-            testProd.setImgUrl("imgUrlTest5");
-            testProd.setPrice("prixTest5");
-            testProd.setTitle("titleTest5");*/
+            Product testProd=new Product();
+            testProd.setAsin("productTest");
+            testProd.setBrand("brandTest");
+            testProd.setImgUrl("imgUrlTest");
+            testProd.setPrice("prixTest");
+            testProd.setTitle("titleTest");
 
            //Ajout du Product
-            //addProduct(testProd,client);
+            addProduct(testProd,client);
 
 			//Modification de données
-			//updateProduct("testInsertion5","title","titletest2",client);
+			updateProduct("productTest","title","titletest2",client);
 
 			//suppression d'un bucket
-			deleteProduct("testInsertion",client);
+			deleteProduct("productTest",client);
 			
 			// Test de QueryApp
-			QueryApp app = new QueryApp(client, cluster);
+			/*QueryApp app = new QueryApp(client, cluster);
 			System.out.println("---------------\n--- QUERY 1 ---\n---------------");
 			app.getPeopleForAsinAndPeriod("B000FIE4WC", "2022-08-28", "2022-09-03");
 			System.out.println("---------------\n--- QUERY 2 ---\n---------------");
-			app.getTotalSalesAndPopularityOfProduct("ice_hockey", "2014");
+			app.getTotalSalesAndPopularityOfProduct("ice_hockey", "2014");*/
 			
 			/*YokozunaIndex famousIndex = new YokozunaIndex("famous");
 			StoreIndex storeIndex = new StoreIndex.Builder(famousIndex).build();
